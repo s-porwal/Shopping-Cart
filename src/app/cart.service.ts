386bdf617dfa20736a1761950827ac6120d0b5ea
Porwal, Shobhit <shobhit.porwal@accenture.com>
2019-08-02 03:29:11 +0530

    Adding all the files

---
+++ src/app/cart.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: "root"
+})
+export class CartService {
+  items = []; // items property to store the list (array) of the current products in the cart
+
+  constructor(private http: HttpClient) {}
+
+  addToCart(product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get("/assets/shipping.json");
+  }
+}
